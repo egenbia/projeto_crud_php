5a8496c89f24f08161343ed56928a6f9de94e3f3
egenbia <105667349+egenbia@users.noreply.github.com>
2024-04-12 11:08:30 -0300

    Add files via upload

---
+++ projeto_crud_php/view/index.php
@@ -0,0 +1,61 @@
+<?php include("blades/top.php"); ?>
+
+<div class="container mt-5">
+<a href="v_cadastro.php" class="btn btn-primary">Cadastrar</a>
+</div>
+<div class="container mt-2 bg-white rounded p-3">
+<table class="table table-bordered table-hover table-striped">
+    <thead>
+        <tr>
+            <th>Código</th>
+            <th>Nome</th>
+            <th>Cidade</th>
+            <th>Curso</th>
+            <th colspan="2">Ação</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>01</td>
+            <td>bia</td>
+            <td>registro</td>
+            <td>ds</td>
+            <td><a href="#" class="btn btn-success">Editar</a></td>
+            <td><a href="#" class="btn btn-danger">Exluir</a></td>
+        </tr>     
+        <tr>
+            <td>01</td>
+            <td>bia</td>
+            <td>registro</td>
+            <td>ds</td>
+            <td><a href="#" class="btn btn-success">Editar</a></td>
+            <td><a href="#" class="btn btn-danger">Exluir</a></td>
+        </tr>  
+        <tr>
+            <td>01</td>
+            <td>bia</td>
+            <td>registro</td>
+            <td>ds</td>
+            <td><a href="#" class="btn btn-success">Editar</a></td>
+            <td><a href="#" class="btn btn-danger">Exluir</a></td>
+        </tr>  
+        <tr>
+            <td>01</td>
+            <td>bia</td>
+            <td>registro</td>
+            <td>ds</td>
+            <td><a href="#" class="btn btn-success">Editar</a></td>
+            <td><a href="#" class="btn btn-danger">Exluir</a></td>
+        </tr>  
+    </tbody>
+    <tfoot>
+        <tr>
+            <td colspan="6">Rodapé</td>
+        </tr>
+    </tfoot>
+</table>
+</div>
+    
+<?php
+include("blades/footer.php");
+?>
---
+++ projeto_crud_php/view/v_cadastro.php
@@ -0,0 +1,21 @@
+<?php include("blades/top.php"); ?>
+
+<div class="container mt-5">
+<a href="index.php" class="btn btn-primary">Voltar</a>
+</div>
+<div class="container mt-2 bg-white rounded p-3">
+   <form action="">
+        <label class="form-label p-3">Nome</label>
+        <input type="text" class="form-control"> 
+        <label class="form-label p-3">Cidade</label>
+        <input type="text" class="form-control"> 
+        <label class="form-label p-3">Curso</label>
+        <input type="text" class="form-control">
+        <p></p>
+        <a href="" class="btn btn-primary">Salvar</a>
+   </form>
+</div>
+    
+<?php
+include("blades/footer.php");
+?>
